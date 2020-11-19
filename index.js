ab37ed184be3b7798cba1174b6ab7444ce72535d
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-11-18 18:55:06 -0800

    set the initial packages

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('hi');
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+var app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>  console.log(`Listening on: http://localhost:${PORT}`); 
+const handleHome = (req, res) => res.send('Hello 1'); 
+const handleProfile = (req, res) =>  res.send("Your are on my profile"); 
+// const betweenHome = (req, res, next) => { // middleware
+//     console.log('between');
+//     next();
+// }
+ 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));  // order is very important! 
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);      
