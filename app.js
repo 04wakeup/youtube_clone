bc6646c65ea4594903942893de2cb857697f2a9e
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-11-23 17:42:25 -0800

    installed and set pug: layout local variables

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localMiddlewares } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
+var app = express();
+//    Middleware
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));  // order is very important! 
+
+
+app.use(localMiddlewares);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);   // my detailed child paths
+app.use(routes.videos, videoRouter);
+
+export default app;
