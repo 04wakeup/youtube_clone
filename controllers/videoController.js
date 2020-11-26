3855df4c5e92da7eb817e296e90eb50880d02bda
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-11-25 23:13:32 -0800

    add Video Upload, Required

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,18 +1,31 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
-    res.render("Home", {pageTitle: "Home", videos}); 
-}
+  res.render("Home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
-    // const searchingBy = req.query.term; sasme as below
-    const {query: {term: searchingBy}} = req;
-    console.log(searchingBy);
-    res.render("Search", {pageTitle: "Search", searchingBy, videos});
-} 
+  // const searchingBy = req.query.term; sasme as below
+  const {
+    query: { term: searchingBy },
+  } = req;
+  console.log(searchingBy);
+  res.render("Search", { pageTitle: "Search", searchingBy, videos });
+};
 
 // export const videos = (req, res) => res.render("Videos", {pageTitle: "Videos"});
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"});
-export const videoDetail = (req, res) => res.render("Video Detail", {pageTitle: "Video Detail"});
-export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"});   
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  // gets info from upload.pug
+  const {
+    body: { file, title, description },
+  } = req;
+  //    To do : Upload and save video
+  res.redirect(routes.videoDetail(11111));
+};
+
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import { upload, editVideo } from "../controllers/videoController";
+import { upload, editVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
 
-const videoRouter = express.Router(); 
+const videoRouter = express.Router();
 
 videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, (req, res) => res.send("Video Detail"));
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Videos"));
 
