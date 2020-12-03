a091c06bea2bce74ae873a9334e1854a1168e238
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-03 13:25:01 -0800

    add Edit Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
 import routes from "../routes";
-import { upload, editVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
+import { upload, getEditVideo, postEditVideo, getUpload, postUpload, videoDetail } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
+videoRouter.get(routes.videos, (req, res) => res.send("Videos")); // need it?
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo); // 1st param: function type
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, (req, res) => res.send("Delete Videos"));
 
 export default videoRouter;
