ca33e71d953ba7b1958f9eab3a418905f16b0457
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-03 00:56:48 -0800

    add Upload movie with multer

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -24,13 +24,17 @@ export const search = (req, res) => {
 // export const videos = (req, res) => res.render("Videos", {pageTitle: "Videos"});
 export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
-  // gets info from upload.pug
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  //    To do : Upload and save video
-  res.redirect(routes.videoDetail(11111));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,7 @@
+import multer from "multer";
 import routes from "./routes";
+
+const multerVideo = multer({ dest: "videos/" }); // upload video and return URL to save on db
 
 export const localMiddlewares = (req, res, next) => {
   res.locals.siteName = "WeTube";
@@ -9,3 +12,5 @@ export const localMiddlewares = (req, res, next) => {
   };
   next();
 };
+
+export const uploadVideo = multerVideo.single("videoFile");
