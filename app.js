2cdee49efaf282dba73e7fc268b8dca339a7117b
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-08 19:05:31 -0800

    add Session on Express

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv"; // hide important info
+dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
-import { localMiddlewares } from "./middlewares";
+import session from "express-session";
+import { localsMiddlewares } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -23,10 +26,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // order is very important!
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET, // from randomkey gen site randomly
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize()); // create info from cookie above(cookieParser)
 app.use(passport.session()); // save session
 
-app.use(localMiddlewares);
+app.use(localsMiddlewares);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); // my detailed child paths
 app.use(routes.videos, videoRouter);
--- middlewares.js
+++ middlewares.js
@@ -3,10 +3,11 @@ import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" }); // upload video and return URL to save on db
 
-export const localMiddlewares = (req, res, next) => {
+export const localsMiddlewares = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes; // it enables to use routes anyware from now
-  res.locals.user = req.user || {};
+  res.locals.user = req.user || null;
+  console.log(req.user);
   next();
 };
 
