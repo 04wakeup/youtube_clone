cbb9361b01b69aa9b7fb57d597111710cc933ad0
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-26 18:56:25 -0800

    change PORT

--- src/init.js
+++ src/init.js
@@ -8,7 +8,7 @@ import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
-const PORT = process.env.PORT || 4000; // find PORT or use default as set
+const PORT = process.env.PORT || 4001; // find PORT or use default as set
 
 const handleListening = () => console.log(`listening on: http://localhost:${PORT}`);
 
